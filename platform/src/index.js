2f39b917e252407d03ecd38b153f323cf49c8be1
prasshinde <prasshinde@paypal.com>
2023-02-10 16:45:22 +0530

    complete import path as variable

--- platform/src/index.js
+++ platform/src/index.js
@@ -5,8 +5,8 @@ import apiRegistry from './apiRegistry.json';
 apiRegistry.forEach(async api => {
 
   app.get(`/api/${api.apiEndpoint}`, async (req, res, next) => {
-    const name = 'businessInfo';
-    const middleware = (await import(`remoteLib/${name}`)).default;
+    const path = 'remoteLib/businessInfo';
+    const middleware = (await import(path)).default;
     middleware(req, res, next);
   })
 });
